51da280bcfeae3b95ba8b27af7dc238df8dd170b
shripati_avasthi <shripatiavasthi@redcliffelabs.com>
2022-11-15 15:03:52 +0530

    ADDED PRESIT

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import * as React from 'react';
 import { Provider } from 'react-redux'
 import store from './src/app/store';
-import MainNaviagtor from './src/app/MainNaviagtor'
+import MainNaviagtor from './src/app/MainNaviagtor';
+import { persistStore } from "redux-persist"
+import { PersistGate } from "redux-persist/integration/react"
+
+
+let persistor = persistStore(store)
 
 function App() {
   return (
     <Provider store={store}>
-      <MainNaviagtor />
+      <PersistGate loading={<Text>Loading....</Text>} persistor={persistor}>
+        <MainNaviagtor />
+      </PersistGate>
     </Provider>
   );
 }
