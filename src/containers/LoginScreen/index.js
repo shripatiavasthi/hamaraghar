cf70b70514f6225951c48b93744368acdfd5904c
Shubham Tandon <shubhamtandon@192.168.1.216>
2024-03-30 20:58:48 +0530

    first commit

--- src/containers/LoginScreen/index.js
+++ src/containers/LoginScreen/index.js
@@ -1,272 +1,289 @@
-import React, { useState, useEffect } from 'react'
-import { Dimensions, StyleSheet, Text, TextInput, View, KeyboardAvoidingView, TouchableOpacity, ImageBackground } from 'react-native'
-import { ScrollView } from 'react-native-gesture-handler';
-import { connect, useDispatch } from "react-redux";
-import { unwrapResult } from '@reduxjs/toolkit'
-import { postLogin } from '../../Slices/LoginSlice'
-import { postUserLogin } from './loginSlice';
-import { alies_exist } from '../../Slices/AliesCheckSlice'
-import { navigate, Screens } from '../../helpers/Screens';
+import React, {useState, useEffect} from 'react';
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+  ImageBackground,
+  Button,
+  Image,
+} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
+import {connect, useDispatch} from 'react-redux';
+import {unwrapResult} from '@reduxjs/toolkit';
+import {postLogin} from '../../Slices/LoginSlice';
+import {postUserLogin} from './loginSlice';
+import {alies_exist} from '../../Slices/AliesCheckSlice';
+import {navigate, Screens} from '../../helpers/Screens';
 import Geolocation from '@react-native-community/geolocation';
-import { validateName, _nameValidate, _passwordvalidate } from '../../helpers/CommonValidator'
-import axios from 'axios'
+import {
+  validateName,
+  _nameValidate,
+  _passwordvalidate,
+} from '../../helpers/CommonValidator';
+import axios from 'axios';
 import IconAntDesign from 'react-native-vector-icons/AntDesign';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
-const { height, width } = Dimensions.get('screen')
+const {height, width} = Dimensions.get('screen');
 
-const image = { image: require("../../staticdata/images/BackgroundImage.png") }
+const image = {image: require('../../staticdata/images/BackgroundImage.png')};
 
-const LoginScreen = (props) => {
+const LoginScreen = props => {
+  const {navigation} = props;
 
-    const { navigation } = props
+  const [Password, setPassword] = useState('');
+  const [errorPassword, setErrorPassword] = useState(null);
+  const [Name, setName] = useState('');
+  const [errorName, setErrorName] = useState(null);
+  const [email, setemail] = useState('');
+  const [errorEmail, setErrorEmail] = useState(null);
 
-    const [Password, setPassword] = useState('');
-    const [errorPassword, setErrorPassword] = useState(null);
-    const [Name, setName] = useState('');
-    const [errorName, setErrorName] = useState(null);
-    const [email, setemail] = useState('');
-    const [errorEmail, setErrorEmail] = useState(null);
+  const [lat, setlat] = useState('');
+  const [lon, setlon] = useState('');
 
+  Geolocation.getCurrentPosition(info => {
+    setlat(info?.coords?.latitude);
+    setlon(info?.coords?.longitude);
+    console.log(info?.coords?.latitude, 'location data ');
+  });
 
-    const [lat, setlat] = useState('')
-    const [lon, setlon] = useState('')
+  useEffect(() => {
+    getlocation();
+  }, [lat && lon]);
 
-    Geolocation.getCurrentPosition(info => {
-        setlat(info?.coords?.latitude)
-        setlon(info?.coords?.longitude)
-        console.log(info?.coords?.latitude, "location data ")
-    }
-    );
+  const getlocation = () => {
+    axios({
+      method: 'post',
+      url: `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=AIzaSyBFB_A5HYqjbBxCd5eLF7oUD7_movLicUk`,
+    })
+      .then(response => {
+        console.log(response, 'direct method');
+      })
+      .catch(e => {
+        console.log(e.response, 'direct method');
+      });
+  };
 
-    useEffect(() => { getlocation() }, [lat && lon])
+  // testing start
+  const handleLogin = () => {
+    // Implement your login logic here
+    console.log('Logging in with:', email, password);
+  };
+  // testing end
 
-    const getlocation = () => {
-        axios({
-            method: 'post',
-            url: `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=AIzaSyBFB_A5HYqjbBxCd5eLF7oUD7_movLicUk`,
-        }).then((response) => {
-            console.log(response, "direct method");
-        }).catch((e) => {
-            console.log(e.response, "direct method")
-        })
-    }
-
-    
-
-    return (
-        <View style={styles.mainContinter}>
-            {/* <ScrollView> */}
-                <ImageBackground source={image.image} style={styles.MainDiv}>
-                    <View style={styles.titleCon}>
-                        <Text style={styles.titTxt}>Log In</Text>
-                    </View>
-                    <KeyboardAvoidingView behavior='position'>
-                        <View style={styles.usercon}>
-                            <TextInput
-                                style={styles.useText}
-                                placeholder='user name'
-                                placeholderTextColor={'#000000'}
-                                onChangeText={async txt => {
-                                    setName(txt),
-                                        setErrorName(_nameValidate(txt))
-                                    // const data = {
-                                    //     query: {},
-                                    //     body: {
-                                    //         "alias": txt,
-                                    //     }
-                                    // }
-                                    // const resp = await props?.aliesexist(data)
-                                    // const rawData = await unwrapResult(resp)
-                                }}
-                            />
-                            {/* {errorName != null ? (
-                                <View
-                                    style={styles.redCon}>
-                                    <Text
-                                        style={styles.redTxt}>
-                                        {errorName}
-                                    </Text>
-                                </View>
-                            ) : null} */}
-                            <View style={styles.passcon}>
-                                <TextInput
-                                    style={styles.useText}
-                                    placeholder='password'
-                                    placeholderTextColor={'#000000'}
-                                    onChangeText={txt => {
-                                        setPassword(txt),
-                                            setErrorPassword(_passwordvalidate(txt))
-                                    }}
-                                />
-                                {/* {errorPassword != null ? (
-                                    <View
-                                        style={styles.redCon}>
-                                        <Text
-                                            style={styles.redTxt}>
-                                            {errorPassword}
-                                        </Text>
-                                    </View>
-                                ) : null} */}
-                            </View>
-                        </View>
-                    </KeyboardAvoidingView>
-                    <View style={styles.forgotCon}>
-                        <TouchableOpacity onPress={() => { alert('Hello') }}>
-                            {/* <Text style={styles.frgTxt}>Forgot your password?</Text> */}
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.titleCons}>
-                        <TouchableOpacity
-                            style={styles.btnCon}
-                            // disabled={errorName && errorPassword ? true : false} 
-                            onPress={async () => {
-                                const data = {
-                                    query: {},
-                                    body: {
-                                        "alias": Name,
-                                        "password": Password,
-                                    }
-                                }
-                                const resp = await props.doLogin(data)
-                                const rawData = await unwrapResult(resp)
-                                console.log(rawData,"kkklll")
-                                if (rawData?.message == 'Success') {
-                                    navigation.push(Screens.Tabs)
-                                } else {
-                                    alert(rawData?.result)
-                                }
-                            }}
-                        >
-                            <Text style={styles.btnTxt}>Log In</Text>
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.forCon}>
-                        <TouchableOpacity onPress={()=>{
-                            navigation.push(Screens.Gender)
-                        }}>
-                            <Text style={styles.anTxt}>SignUp!</Text>
-                        </TouchableOpacity>
-                    </View>
-                </ImageBackground>
-            {/* </ScrollView> */}
+  return (
+    <View style={styles.container}>
+      <View style={styles.logocontainer}>
+        <Image
+          source={require('../../staticdata/images/IMG-20231104-WA0002-removebg-preview 1.png')}
+          style={{height: 300, width: 300}}
+        />
+      </View>
+      <View style={styles.subcontainer}>
+        <Text style={styles.title}>Sign In to Hamaragharr</Text>
+        <View
+          style={{
+            width: '100%',
+            height: 50,
+            borderWidth: 1,
+            borderColor: '#ccc',
+            borderRadius: 5,
+            paddingHorizontal: 10,
+            marginBottom: 10,
+          }}>
+          <Text style={{fontSize: 14, color: '#7D8592', fontWeight: '700'}}>
+            Email Address
+          </Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            //  onChangeText={text => setEmail(text)}
+            //   value={email}
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
         </View>
-    )
-}
+        <View>
+          <Text style={{fontSize: 14, color: '#7D8592', fontWeight: '700'}}>
+            Email Address
+          </Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            //  onChangeText={text => setPassword(text)}
+            //  value={password}
+            secureTextEntry
+          />
+        </View>
+        <Button title="Login" onPress={handleLogin} />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    mainContinter: {
-        flex: 1,
-        // backgroundColor: 'cyan',
-    },
-    MainDiv: {
-        height: height,
-        width: width / 1,
-        // backgroundColor: 'lightblue',
-    },
-    titleCon: {
-        height: height / 7,
-        width: width / 1.4,
-        // backgroundColor: 'cyan',
-        justifyContent: 'flex-end',
-        // alignItems: 'center',    
-        alignSelf: 'center'
+  container: {
+    flex: 1,
+    // justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: 'lightblue',
+  },
+  logocontainer: {
+    marginTop: '16%',
+    backgroundColor: 'pink',
+    height: '14%',
+    width: '90%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  subcontainer: {
+    backgroundColor: 'yellow',
+    flexGrow: 0.6,
+    width: '90%',
+    padding : 20,
+    //  justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 30,
+    opacity: 0.7, // Opacity (0 is transparent, 1 is opaque)
+    shadowColor: '#000', // Shadow color
+    shadowOffset: {width: 0, height: 2}, // Shadow offset (width, height)
+    shadowOpacity: 0.25, // Shadow opacity (0 is transparent, 1 is opaque)
+    shadowRadius: 3, // Shadow radius
+    elevation: 5, // Elevation for Android (affects shadow appearance)
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '700',
+    marginBottom: 20,
+    color: '#0A1629',
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  // mainContinter: {
+  //     flex: 1,
+  //     // backgroundColor: 'cyan',
+  // },
+  // MainDiv: {
+  //     height: height,
+  //     width: width / 1,
+  //     // backgroundColor: 'lightblue',
+  // },
+  // titleCon: {
+  //     height: height / 7,
+  //     width: width / 1.4,
+  //     // backgroundColor: 'cyan',
+  //     justifyContent: 'flex-end',
+  //     // alignItems: 'center',
+  //     alignSelf: 'center'
 
-    },
-    titTxt: {
-        fontSize: height / 20,
-        // fontFamily: Montserrat,
-        fontWeight: '400',
-        color: '#FFFFFF'
-    },
-    usercon: {
-        height: height / 1.8,
-        width: width / 1.4,
-        // backgroundColor: 'cyan',
-        justifyContent: 'center',
-        alignItems: 'center',    
-        alignSelf: 'center',
-    },
-    useText: {
-        height: height / 16,
-        width: width / 1.4,
-        marginBottom : 30,
-        backgroundColor: '#FFFFFF',
-        paddingHorizontal: 20,
-        fontSize: height / 55,
-        borderWidth: 0.5,
-        borderRadius: 10
-    },
-    forgotCon: {
-        height: height / 30,
-        width: width / 1.4,
-        // backgroundColor: 'cyan',
-        alignSelf: 'center',
-        justifyContent: 'center',
-        alignItems: 'flex-end'
-    },
-    frgTxt: {
-        fontSize: 13,
-        fontWeight: '400',
-        color: '#FFFFFF'
-    },
-    btnCon: {
-        height: height / 16,
-        width: width / 1.4,
-        backgroundColor: '#FF4500',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    btnTxt: {
-        color: '#FFFFFF',
-        fontSize: 20
-    },
-    titleCons: {
-        height: height / 15,
-        width: width / 1.4,
-        // backgroundColor: 'cyan',
-        justifyContent: 'flex-end',
-        // alignItems: 'center',    
-        alignSelf: 'center'
-    },
-    forCon: {
-        height: height / 27,
-        width: width / 1.4,
-        // backgroundColor: 'cyan',
-        alignSelf: 'center',
-        justifyContent: 'flex-end',
-        alignItems: 'baseline'
-    },
-    anTxt: {
-        color: '#707070',
-        fontSize: 18,
-        fontWeight:'bold',
+  // },
+  // titTxt: {
+  //     fontSize: height / 20,
+  //     // fontFamily: Montserrat,
+  //     fontWeight: '400',
+  //     color: '#FFFFFF'
+  // },
+  // usercon: {
+  //     height: height / 1.8,
+  //     width: width / 1.4,
+  //     // backgroundColor: 'cyan',
+  //     justifyContent: 'center',
+  //     alignItems: 'center',
+  //     alignSelf: 'center',
+  // },
+  // useText: {
+  //     height: height / 16,
+  //     width: width / 1.4,
+  //     marginBottom : 30,
+  //     backgroundColor: '#FFFFFF',
+  //     paddingHorizontal: 20,
+  //     fontSize: height / 55,
+  //     borderWidth: 0.5,
+  //     borderRadius: 10
+  // },
+  // forgotCon: {
+  //     height: height / 30,
+  //     width: width / 1.4,
+  //     // backgroundColor: 'cyan',
+  //     alignSelf: 'center',
+  //     justifyContent: 'center',
+  //     alignItems: 'flex-end'
+  // },
+  // frgTxt: {
+  //     fontSize: 13,
+  //     fontWeight: '400',
+  //     color: '#FFFFFF'
+  // },
+  // btnCon: {
+  //     height: height / 16,
+  //     width: width / 1.4,
+  //     backgroundColor: '#FF4500',
+  //     justifyContent: 'center',
+  //     alignItems: 'center'
+  // },
+  // btnTxt: {
+  //     color: '#FFFFFF',
+  //     fontSize: 20
+  // },
+  // titleCons: {
+  //     height: height / 15,
+  //     width: width / 1.4,
+  //     // backgroundColor: 'cyan',
+  //     justifyContent: 'flex-end',
+  //     // alignItems: 'center',
+  //     alignSelf: 'center'
+  // },
+  // forCon: {
+  //     height: height / 27,
+  //     width: width / 1.4,
+  //     // backgroundColor: 'cyan',
+  //     alignSelf: 'center',
+  //     justifyContent: 'flex-end',
+  //     alignItems: 'baseline'
+  // },
+  // anTxt: {
+  //     color: '#707070',
+  //     fontSize: 18,
+  //     fontWeight:'bold',
 
+  // },
+  // redCon: {
+  //     height: height * 0.02,
+  //     width: width / 1.4,
+  //     // backgroundColor: 'green',
+  //     alignSelf: 'center'
+  // },
+  // redTxt: {
+  //     color: 'red',
+  //     fontSize: 13,
+  // }
+});
+
+const mapStateToProps = state => ({
+  login: state.LoginSlice,
+});
+
+const mapDispatchToProps = dispatch => {
+  return {
+    doLogin: data => {
+      return dispatch(postUserLogin(data));
     },
-    redCon: {
-        height: height * 0.02,
-        width: width / 1.4,
-        // backgroundColor: 'green',
-        alignSelf: 'center'
+    aliesexist: data => {
+      return dispatch(alies_exist(data));
     },
-    redTxt: {
-        color: 'red',
-        fontSize: 13,
-    }
-})
+  };
+};
 
-const mapStateToProps = (state) => ({
-    login: state.LoginSlice
-})
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        doLogin: (data) => {
-            return dispatch(postUserLogin(data));
-        },
-        aliesexist: (data) => {
-            return dispatch(alies_exist(data));
-        }
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(LoginScreen)
+export default connect(mapStateToProps, mapDispatchToProps)(LoginScreen);
